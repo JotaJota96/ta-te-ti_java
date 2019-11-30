9738d68d611814547b3224f64bf86f20f9f9fa0d
JotaJota96 <jjap96@gmail.com>
2019-11-30 16:57:11 -0300

    Tablero definido

--- src/ta/te/ti/TaTeTi.java
+++ src/ta/te/ti/TaTeTi.java
@@ -3,7 +3,19 @@ package ta.te.ti;
 public class TaTeTi {
     
     public static void main(String[] args) {
-        
+        Tablero t = new Tablero();
+        int cont = 1;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                t.set(i, j, String.valueOf(cont++).charAt(0));
+            }
+        }
+        System.out.println(t.toString(0));
+        System.out.println();
+        System.out.println(t.toString(1));
+        System.out.println();
+        System.out.println(t.toString(2));
+        System.out.println();
         
     }
     
---
+++ src/ta/te/ti/Tablero.java
@@ -0,0 +1,86 @@
+package ta.te.ti;
+
+public class Tablero {
+
+    private static final int TAMANIO = 3;
+    char tablero[][];
+
+    public Tablero() {
+        tablero = new char[TAMANIO][TAMANIO];
+    }
+
+    public char[][] getTablero() {
+        char tab[][] = new char[TAMANIO][TAMANIO];
+        for (int i = 0; i < TAMANIO; i++) {
+            for (int j = 0; j < TAMANIO; j++) {
+                tab[i][j] = this.tablero[i][j];
+            }
+        }
+        return tab;
+    }
+
+    public void set(int x, int y, char c) {
+        if (x < 0 || TAMANIO <= x || y < 0 || TAMANIO <= y) {
+            throw new RuntimeException("Coordenada [x,y] = [,] fuera de rango");
+        }
+        tablero[x][y] = c;
+    }
+
+    @Override
+    public String toString() {
+        String ret = "{";
+        for (int i = 0; i < TAMANIO; i++) {
+            ret += "{";
+            for (int j = 0; j < TAMANIO; j++) {
+                ret += this.tablero[i][j];
+                if (j + 1 < TAMANIO) {
+                    ret += ", ";
+                }
+            }
+            ret += "}";
+            if (i + 1 < TAMANIO) {
+                ret += ", ";
+            }
+        }
+        ret += "}";
+        return ret;
+    }
+
+    public String toString(int modo) {
+        String ret = "";
+
+        switch (modo) {
+            case 0:
+                ret = this.toString();
+                break;
+            case 1:
+                ret += lineaHorizontal() + "\n";
+                for (int i = 0; i < TAMANIO; i++) {
+                    for (int j = 0; j < TAMANIO; j++) {
+                        ret += "| " + this.tablero[i][j] + " ";
+                    }
+                    ret += "|\n";
+                    ret += lineaHorizontal() + "\n";
+                }
+                break;
+            case 2:
+                for (int i = 0; i < TAMANIO; i++) {
+                    for (int j = 0; j < TAMANIO; j++) {
+                        ret += this.tablero[i][j] + ":";
+                    }
+                    ret += ";";
+                }
+                break;
+        }
+        return ret;
+    }
+
+    private String lineaHorizontal() {
+        String ret = "";
+        for (int i = 0; i < TAMANIO; i++) {
+            ret += "+---";
+        }
+        ret += "+";
+        return ret;
+    }
+}
